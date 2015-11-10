dc72ec34de92387ff1fecffc714df975297a64f5
ishark <isha@datatorrent.com>
2015-11-09 16:29:46 -0800

    Removed comments in job main

--- linear-road/src/main/java/com/flink/linearroad/Job.java
+++ linear-road/src/main/java/com/flink/linearroad/Job.java
@@ -42,30 +42,9 @@ public class Job {
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
 
-		/**
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataSet<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/programming_guide.html
-		 *
-		 * and the examples
-		 *
-		 * http://flink.apache.org/docs/latest/examples.html
-		 *
-		 */
+		//DataStream<>
 
 		// execute program
-		env.execute("Flink Java API Skeleton");
+		env.execute("Linear road application in Flink");
 	}
 }
